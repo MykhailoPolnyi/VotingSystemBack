c6c9c5e3268bd6d8d48a5964a3204b85d011f4b7
mmartsiv <v.mariya.marchiv@gmail.com>
2023-06-13 17:05:53 +0300

    implement repository layer

--- src/main/java/ua/lviv/iot/repository/ElectionResultRepository.java
+++ src/main/java/ua/lviv/iot/repository/ElectionResultRepository.java
@@ -2,8 +2,13 @@ package ua.lviv.iot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ua.lviv.iot.model.election.Election;
 import ua.lviv.iot.model.election.result.ElectionResult;
+
+import java.util.List;
 
 @Repository
 public interface ElectionResultRepository extends JpaRepository<ElectionResult, Long> {
+    Integer sumVoteNumberByElectionId(Integer electionId);
+    List<ElectionResult> findAllByElectionId(Integer electionId);
 }
--- src/main/java/ua/lviv/iot/repository/UserRepository.java
+++ src/main/java/ua/lviv/iot/repository/UserRepository.java
@@ -2,8 +2,12 @@ package ua.lviv.iot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ua.lviv.iot.model.election.result.ElectionResult;
 import ua.lviv.iot.model.user.User;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByIdentityCode(String code);
 }
